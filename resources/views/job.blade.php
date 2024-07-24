913931a4b57c1ec1c2b607723edef64e8bd8cd72
Patrick Bandeira <patrick010509@gmail.com>
2024-07-24 18:20:40 -0300

    adiciona eager loading nas queries de job

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -3,6 +3,10 @@
         Job
     </x-slot:heading>
 
+    <h2 class="h6 mb-3">
+        {{ $job->employer->name }}
+    </h2>
+
     <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
 
     <p>
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,14 +2,17 @@
     <h1>
         Hello from Jobs page
     </h1>
-    <ul class="row gy-1 list-unstyled">
+    <ul class="row gy-2 list-unstyled">
         @foreach($jobs as $job)
-            <li class="col-md-auto">
-                <a href="/jobs/{{ $job['id'] }}" class="text-decoration-none btn btn-sm btn-info">
-                    <h2 class="h5">
+            <li class="col-md-3">
+                <a href="/jobs/{{ $job['id'] }}" class="text-decoration-none btn btn-sm btn-outline-secondary w-100 h-100">
+                    <h2 class="h6 mb-3">
+                        {{ $job->employer->name }}
+                    </h2>
+                    <h2 class="h4">
                         {{ $job['title'] }}
                     </h2>
-                    <div>
+                    <div class="fs-5">
                         receives {{ $job['salary'] }} per year
                     </div>
                 </a>

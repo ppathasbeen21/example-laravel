e309f3e8b69072adae1df9db64b7e908c841d65e
Patrick Bandeira <patrick010509@gmail.com>
2024-07-26 16:57:40 -0300

    feat (tag): apaga tags e factories

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -20,7 +20,7 @@ class JobFactory extends Factory
         return [
             'title' => fake()->jobTitle(),
             'employer_id' => Employer::factory(),
-            'salary' => 'R$' . fake()->numberBetween(1000, 10000),
+            'salary' => fake()->numberBetween(1000, 10000),
         ];
     }
 }

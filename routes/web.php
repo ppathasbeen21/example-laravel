6321f29f2691c3f96c95f3a1e5c00e5eec1f9c2b
Patrick Bandeira <patrick010509@gmail.com>
2024-07-31 15:42:00 -0300

    feat (routes): ajusta

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,7 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/','index');
-
-//Route::controller(JobController::class)->group(function () {
-//    Route::get('/jobs', 'index');
-//    Route::get('/jobs/{job}', 'show');
-//    Route::get('/jobs/create', 'create');
-//    Route::post('/jobs', 'store');
-//    Route::get('/jobs/{job}/edit', 'edit');
-//    Route::patch('/jobs/{job}', 'update');
-//    Route::delete('/jobs/{job}', 'destroy');
-//});
-Route::resource('jobs', JobController::class);
-
-Route::get('/about', function () {
-    return view('about');
-});
-
+Route::view('/about','about');
 Route::view('/contact','contact');
+
+Route::resource('jobs', JobController::class);
